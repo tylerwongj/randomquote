acf49092290803416279a888f6945f97077085f3
Tyler Wong <tylerwong.j@gmail.com>
2020-05-21 22:50:30 -0700

    master - randomquote created

--- main.js
+++ main.js
@@ -13,10 +13,14 @@
 
 var unirest = require("unirest");
 
-var req = unirest("GET", "https://quotes21.p.rapidapi.com/quote");
+var req = unirest("GET", "https://quotes15.p.rapidapi.com/quotes/random/");
+
+req.query({
+	"language_code": "en"
+});
 
 req.headers({
-	"x-rapidapi-host": "quotes21.p.rapidapi.com",
+	"x-rapidapi-host": "quotes15.p.rapidapi.com",
 	"x-rapidapi-key": "7ed07217e3msh6a74043b505016fp13952ajsnecaa1e97b458",
 	"useQueryString": true
 });
@@ -25,5 +29,7 @@ req.headers({
 req.end(function (res) {
 	if (res.error) throw new Error(res.error);
 
-	console.log(res.body);
+  // console.log(res.body);
+  console.log(`"${res.body.content}"`);
+  console.log(`- ${res.body.originator.name}`);
 });
