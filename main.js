fb2cac6fca7e4ae7c46e2053ca372a7800ad4f97
Tyler Wong <tylerwong.j@gmail.com>
2020-05-21 22:42:17 -0700

    master - unirest for rapidAPI quote

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-const url="http://quotes.stormconsultancy.co.uk/random.json";
+// const url="http://quotes.stormconsultancy.co.uk/random.json";
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(`"${data.quote}"`);
-    console.log(`- ${data.author}`);
-  });
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(`"${data.quote}"`);
+//     console.log(`- ${data.author}`);
+//   });
 
 
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://quotes21.p.rapidapi.com/quote");
+
+req.headers({
+	"x-rapidapi-host": "quotes21.p.rapidapi.com",
+	"x-rapidapi-key": "7ed07217e3msh6a74043b505016fp13952ajsnecaa1e97b458",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
